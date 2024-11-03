b136d85ec0e68a6bf7099a4454684b4d2d18ee30
Yulya Terenteva <yulya.terenteva.2005@internet.ru>
2024-11-03 17:49:46 +0300

    second

---
+++ src/masks.py
@@ -0,0 +1,30 @@
+from typing import Union
+
+
+def get_mask_card_number(num_of_card: Union[int, str]) -> str:
+    """функция ввода номера карты"""
+    num_of_card = str(num_of_card)
+    # превращаем в строку
+    masked_num_of_card = num_of_card[-16:-10] + "******" + num_of_card[-4:]
+    result = " ".join(
+        [masked_num_of_card[-16:-12], masked_num_of_card[-12:-8], masked_num_of_card[-8:-4], masked_num_of_card[-4:]]
+    )
+
+    return result
+
+
+
+
+
+
+def get_mask_account(account_number: Union[int, str]) -> str:
+    """функция ввода номера счета"""
+    account_number = str(account_number)
+    # превращаем в строку
+    masked_acc_num = account_number[-20:-6] + "**" + account_number[-4:]
+    result_1 = "".join([masked_acc_num[-6:-4], masked_acc_num[-4:]])
+
+    return result_1
+
+
+
--- src/widget.py
+++ src/widget.py
@@ -23,3 +23,5 @@ print(mask_account_card(first_num))
 
 
 
+
+
