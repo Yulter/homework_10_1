39ca8781e082768135320fc2ff8898332f875826
Yulya Terenteva <yulya.terenteva.2005@internet.ru>
2024-11-28 22:33:25 +0300

    hw2 final done

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+
+@pytest.fixture
+def card_number_fixture():
+    return "7000792289606361"
+
+@pytest.fixture
+def num_of_account_fixture():
+    return "64686473678894779589"
+
+
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,14 +1,9 @@
 import pytest
-
 from src.masks import get_mask_account, get_mask_card_number
 
 
-@pytest.mark.parametrize(
-    "card_number, correct_number",
-    [("7000792289606361", "7000 79** **** 6361"), ("9000792289606361", "9000 79** **** 6361")],
-)
-def test_get_mask_card_number(card_number, correct_number):
-    assert get_mask_card_number(card_number) == correct_number
+def test_get_mask_card_number(card_number_fixture):
+    assert get_mask_card_number(card_number_fixture) == "7000 79** **** 6361"
 
 
 def test_len_card_number():
@@ -25,11 +20,8 @@ def test_type_card_number():
     assert str(exc_info.value) == "Неверный тип строки"
 
 
-@pytest.mark.parametrize(
-    "num_of_account, correct_account_number", [("64686473678894779589", "**9589"), ("35383033474447895560", "**5560")]
-)
-def test_get_mask_account(num_of_account, correct_account_number):
-    assert get_mask_account(num_of_account) == correct_account_number
+def test_get_mask_account(num_of_account_fixture):
+    assert get_mask_account(num_of_account_fixture) == "**9589"
 
 
 def test_len_account_number():
